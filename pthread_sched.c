019ebdd866ca445096750c0d1363268c4cda1b8e
rishav21556 <108609833+rishav21556@users.noreply.github.com>
2022-11-20 18:15:50 +0530

    Add files via upload

--- pthread_sched.c
+++ pthread_sched.c
@@ -15,7 +15,6 @@ void *count1(void *num){
     struct timespec getTime1;
     clock_gettime(CLOCK_REALTIME,&getTime1);
     struct timespec getTimeFinal;
-    //printf("h\n");
     pthread_t id = pthread_self();
     setpriority(PRIO_PROCESS,0,-20);
     param.sched_priority = 0;
@@ -23,44 +22,52 @@ void *count1(void *num){
         printf("Error A\n");
     }
     long long int n =pow(2,32);
-    for (int i=1;i<4294967296;i++){
-        //printf("%d\n",i);
+    int i  = 1;
+    while(i<n){   // 4294967296
+        i++;
     }
     clock_gettime(CLOCK_REALTIME,&getTimeFinal);
-    printf("Time taken by SCHED_OTHER -->%f\n",(double)(getTimeFinal.tv_sec)-(double)(getTime1.tv_sec));
+    printf("Time taken by SCHED_OTHER_SCHEDULING --> %f\n",(getTimeFinal.tv_sec+(getTimeFinal.tv_nsec/(double)1000000000))-(getTime1.tv_sec+(getTime1.tv_nsec/(double)1000000000)));
 }
 void *count2(void *num){
-    struct sched_param param;
     struct timespec getTime2;
+    struct sched_param param;
     clock_gettime(CLOCK_REALTIME,&getTime2);
-    struct timespec getTimeFinal;
+    
     pthread_t id = pthread_self();
-    param.sched_priority = 10;
+    param.sched_priority = 0;
     if (pthread_setschedparam(id,SCHED_RR,&param)!=0){
         printf("Error B\n");
     }
     
     long long int n =pow(2,32);
-    for (int i=1;i<4294967296;i++){
+    long long int i = 1;
+    while(i<n){
+        i++;
     }
+    struct timespec getTimeFinal;
     clock_gettime(CLOCK_REALTIME,&getTimeFinal);
-    printf("Time taken by RR_SCHEDULING (10) -->%f\n",(double)(getTimeFinal.tv_sec)-(double)(getTime2.tv_sec));
+    printf("Time taken by RR_SCHEDULING (10) --> %f\n",(getTimeFinal.tv_sec+(getTimeFinal.tv_nsec/(double)1000000000))-(getTime2.tv_sec+(getTime2.tv_nsec/(double)1000000000)));
 }
 void *count3(void *num){
-    struct sched_param param;
     struct timespec getTime2;
+    struct sched_param param;
     clock_gettime(CLOCK_REALTIME,&getTime2);
-    struct timespec getTimeFinal;
+    
     pthread_t id = pthread_self();
-    param.sched_priority = 11;
+    param.sched_priority = 0;
     if (pthread_setschedparam(id,SCHED_FIFO,&param)!=0){
         printf("Error C\n");
     }
-    long long int n =  pow(2,32);
-    for(int i=1;i<4294967296;i++){
+    
+    long long int n =pow(2,32);
+    long long int i = 1;
+    while(i<n){
+        i++;
     }
+    struct timespec getTimeFinal;
     clock_gettime(CLOCK_REALTIME,&getTimeFinal);
-    printf("Time taken by FIFO_SCHEDULING -->%f\n",(double)(getTimeFinal.tv_sec)-(double)(getTime2.tv_sec));
+printf("Time taken by FIFO_SCHEDULING --> %f\n",(getTimeFinal.tv_sec+(getTimeFinal.tv_nsec/(double)1000000000))-(getTime2.tv_sec+(getTime2.tv_nsec/(double)1000000000)));
 }
 
 
